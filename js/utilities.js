5ec317df1af95abc9114c8ae412d7dee56cb62ee
ansaryshohan <ansarye2083@gmaail.com>
2022-10-01 02:55:57 +0600

    alert massage added

--- js/utilities.js
+++ js/utilities.js
@@ -1,18 +1,23 @@
-function getInputValue(elementId){
-  const inputElement= document.getElementById(elementId);
-  const inputElementString= inputElement.value;
-  const inputElementAmount= parseFloat(inputElementString);
- return inputElementAmount;
+function getInputValue(elementId) {
+  const inputElement = document.getElementById(elementId);
+  const inputElementString = inputElement.value;
+  const inputElementAmount = parseFloat(inputElementString);
+  if (isNaN(inputElementAmount)) {
+    alert('please provide a cost amount');
+  }
+  else {
+    return inputElementAmount
+  }
 }
 
-function getElementInnerText(elementId){
-  const getElement= document.getElementById(elementId);
-  const elementString= getElement.innerText;
-  const elementAmount= parseFloat(elementString);
- return elementAmount;
+function getElementInnerText(elementId) {
+  const getElement = document.getElementById(elementId);
+  const elementString = getElement.innerText;
+  const elementAmount = parseFloat(elementString);
+  return elementAmount;
 }
 
-function setElementValue(elementId, value){
+function setElementValue(elementId, value) {
   const theElement = document.getElementById(elementId);
-  theElement.innerText= value;
+  theElement.innerText = value;
 }
